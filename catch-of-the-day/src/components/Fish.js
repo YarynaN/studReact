818354ed2dddeac1008ae141a4eb26863487cf8a
Yaryna Nepyivoda <yaryna.nepyivoda@gmail.com>
2018-04-26 15:09:12 +0300

    JSX displaying order
    Fixed displaying order.

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -8,7 +8,9 @@ class Fish extends React.Component{
 		return <li className="menu-fish">
 			<img src = {image} alt = {name} />
 			<h3 className= "fish-name">{name}
-				<span className="price">{formatPrice(price)}</span>
+				<span className="price">
+					{formatPrice(price)}
+				</span>
 			</h3>
 			<p>{desc}</p>
 			<button disabled={!isAvailable} onClick = { () => this.props.addToOrder(this.props.index)}> {isAvailable ? 'Add To Cart' : 'Sold Out!'} </button>
