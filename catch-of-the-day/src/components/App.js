3617fc02c8a3e952f99c0fb30d5e9df3efd7b886
Yaryna Nepyivoda <yaryna.nepyivoda@gmail.com>
2018-05-10 16:50:08 +0300

    Bi-directional Data flow & removing
    Some editing added.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -15,14 +15,27 @@ class App extends React.Component{
 
 	componentDidMount(){
 		const { params } = this.props.match;
+		const localStorageRef = localStorage.getItem(params.storeId);
+		if (localStorageRef){
+			this.setState({order : JSON.parse(localStorageRef)})
+		}
 		this.ref = base.syncState(`${params.storeId}/fishes`, {
 			context : this,
 			state: 'fishes'
 		});
 	}
+
+	componentDidUpdate(){
+		localStorage.setItem(
+		 this.props.match.params.storeId,
+		 JSON.stringify(this.state.order)
+		);
+	}
+
 	componentWillUnmount(){
 		base.removeBinding(this.ref);
 	}
+
 
 	addFish = (fish) => {
 		const fishes = {...this.state.fishes};
@@ -30,6 +43,19 @@ class App extends React.Component{
 		this.setState({
 			fishes: fishes
 		});
+	};
+
+	updateFish = (key, updatedFish) => {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({fishes : fishes});
+
+	};
+
+	deleteFish = (key) => {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null;
+		this.setState({fishes});
 	};
 
 	loadSampleFishes = () =>{
@@ -40,6 +66,11 @@ class App extends React.Component{
 		const order = {...this.state.order};
 		order[key] = order[key] + 1 || 1;
 		this.setState({ order });
+	};
+	removeFromOrder = (key) => {
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({order});
 	}
 
 	render(){
@@ -56,8 +87,16 @@ class App extends React.Component{
 							addToOrder = {this.addToOrder} />))}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order}/>
-				<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+					<Order 
+						fishes={this.state.fishes} 
+						order={this.state.order}
+						removeFromOrder={this.removeFromOrder}/>
+					<Inventory 
+						addFish={this.addFish} 
+						updateFish={this.updateFish} 
+						deleteFish={this.deleteFish} 
+						loadSampleFishes={this.loadSampleFishes}
+						fishes = {this.state.fishes}/>
 			</div>
 			)
 	}
